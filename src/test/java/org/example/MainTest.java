eb84fa4470eb0b01c8093a3aa396c40cc588558a
Ralph <rpbarczok@gmx.de>
2025-07-01 12:28:20 +0200

    add Length validation and Test

---
+++ src/test/java/org/example/MainTest.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+    String pwGood = "sR12hcH:op";
+    String pwShort = "alös";
+    String pwNoNum = "dtSRfkjlFK";
+    String pwNoCap = "dtsr4fkjlfk";
+    String pwNoLow = "DTRS4FKJASD";
+
+    @Test
+    void valLen_shouldReturnTrue_WhenInputGE8() {
+        boolean result = Main.valLen(pwGood);
+        assertTrue(result);
+    }
+
+    @Test
+    void valLen_shouldReturnFalse_WhenInputLT8() {
+        boolean result = Main.valLen(pwShort);
+        assertFalse(result);
+    }
+}
